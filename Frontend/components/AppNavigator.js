58d09d9c8b00fe1f6aeb447e19991a5256b096e4
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-07 16:28:05 +0530

    Add Login and Register screens to AppNavigator

--- Frontend/components/AppNavigator.js
+++ Frontend/components/AppNavigator.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import welcome1 from '../components/welcome1';
 import welcome2 from '../components/welcome2';
 import welcome3 from '../components/welcome3';
+import Login from '../components/Login';
+import Register from '../components/Register';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +14,8 @@ const AppNavigator = () => {
       <Stack.Screen name="welcome1" component={welcome1} />
       <Stack.Screen name="welcome2" component={welcome2} />
       <Stack.Screen name="welcome3" component={welcome3} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 };
