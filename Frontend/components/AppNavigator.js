6543ce925567b6782e16b571a053f74362e1dbfb
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-07 14:27:58 +0530

    Add welcome2 and welcome3 screens to AppNavigator

--- Frontend/components/AppNavigator.js
+++ Frontend/components/AppNavigator.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import welcome1 from '../components/welcome1';
+import welcome2 from '../components/welcome2';
+import welcome3 from '../components/welcome3';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +10,8 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="welcome1" component={welcome1} />
+      <Stack.Screen name="welcome2" component={welcome2} />
+      <Stack.Screen name="welcome3" component={welcome3} />
     </Stack.Navigator>
   );
 };
