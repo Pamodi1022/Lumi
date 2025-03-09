4fb3a9485834e5988d971a898867783bf9d3d832
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-09 20:15:30 +0530

    Update JWT token expiration time from 1 hour to 24 hours

--- Backend/config/jwtConfig.js
+++ Backend/config/jwtConfig.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('./dotenvConfig');
 
 const generateToken = (email) => {
-  return jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ email }, JWT_SECRET, { expiresIn: '24h' });
 };
 
 module.exports = generateToken;
