3bcc850fb158b3fe796b74f4cd055721358c7fdb
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-09 16:37:03 +0530

    Add Firebase configuration and initialization for web app

---
+++ Frontend/components/FirebaseConfig.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUZbRR3xhCksOUfrw5ArIj8lwBnkEORk4",
+  authDomain: "lumi-1022.firebaseapp.com",
+  projectId: "lumi-1022",
+  storageBucket: "lumi-1022.firebasestorage.app",
+  messagingSenderId: "511132581535",
+  appId: "1:511132581535:web:d080f4e4a70e8f17c7acaa",
+  measurementId: "G-GDYQNDR0TM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { storage, db, analytics };
+
