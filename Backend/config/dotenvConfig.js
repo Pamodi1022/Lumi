7a18073b4a49749bbb781d29fd45d9fd538c495d
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-03-09 20:15:18 +0530

    Update dotenv configuration to provide a default JWT secret

--- Backend/config/dotenvConfig.js
+++ Backend/config/dotenvConfig.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || "bdabsdaiwhei232738&*&@W@*!GHWUBdshjbdcehjyiu3i3urd",
 };
