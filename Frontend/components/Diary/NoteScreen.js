ef6dbd4f7420f20e8b05b48c32f84421f2230591
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-04-22 23:48:36 +0530

    Refactor NoteScreen.js for consistent formatting and style

--- Frontend/components/Diary/NoteScreen.js
+++ Frontend/components/Diary/NoteScreen.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StatusBar, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import styles from '../../Styles/NoteScreenStyles'; // Adjust the import path as necessary
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import styles from "../../Styles/NoteScreenStyles"; // Adjust the import path as necessary
 
 const NoteScreen = () => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#d2cec0" barStyle="dark-content" />
-      
+
       {/* Header */}
       <View style={styles.header}>
         <TouchableOpacity style={styles.backButton}>
           <Ionicons name="arrow-back" size={24} color="black" />
         </TouchableOpacity>
-        
+
         <View style={styles.headerButtons}>
           <TouchableOpacity style={styles.iconButton}>
             <Ionicons name="trash-outline" size={24} color="white" />
@@ -23,7 +30,7 @@ const NoteScreen = () => {
           </TouchableOpacity>
         </View>
       </View>
-      
+
       {/* Note Content */}
       <View style={styles.noteContent}>
         <Text style={styles.titleText}>Title</Text>
